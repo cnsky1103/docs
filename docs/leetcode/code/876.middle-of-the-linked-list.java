21e3de4c72dc0c1a5bce063db9eca9576862c2d5
cnsky1103 <cnsky1103@gmail.com>
2023-03-12 16:17:54 -0400

    双指针链表

---
+++ docs/leetcode/code/876.middle-of-the-linked-list.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=876 lang=java
+ *
+ * [876] Middle of the Linked List
+ */
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
+// @lc code=end
